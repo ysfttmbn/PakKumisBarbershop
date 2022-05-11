f0d1110edaa77a164aeef14faa90525a3f28aa74
hiskiaphsp <103241802+hiskiaphsp@users.noreply.github.com>
2022-05-11 14:16:47 +0700

    Update index.blade.php

--- resources/views/pages/coupon/index.blade.php
+++ resources/views/pages/coupon/index.blade.php
@@ -1,5 +1,26 @@
 @extends('theme.main')
 
 @section('content')
-
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special Discount</h5>
+                  <p class="card-text">Lorem Ipsum</p>
+                  <a href="#" class="btn btn-primary">Use</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special Discount</h5>
+                  <p class="card-text">Lorem Ipsum</p>
+                  <a href="#" class="btn btn-primary">Use</a>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
 @endsection

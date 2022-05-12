263c59779fb0b73c30689eca2f891e0532b801fa
hiskiaphsp <103241802+hiskiaphsp@users.noreply.github.com>
2022-05-12 13:47:49 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('auth', AuthController::class);
 Route::get('login', function () {
     return view('pages.login.index');
 });
